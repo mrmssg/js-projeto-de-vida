9e1a618f462f5adacbe37e47b79b0968a1d8f771
Mauro S S Goncalves <133293974+mrmssg@users.noreply.github.com>
2025-05-26 19:12:02 +0000

    estilização inicial do counter

--- main.js
+++ main.js
@@ -15,10 +15,24 @@ for(let i=0;i <botoes.length;i++){
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-05-20T00:00:00"); 
+const tempoObjetivo1 = new Date("2025-10-06T00:00:00"); 
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 60);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  return dias + " dias " +  horas + " horas " + minutos + " minutos " + segundos + " segundos";
+  }
 
 
 
