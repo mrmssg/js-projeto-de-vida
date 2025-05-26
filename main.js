fce98778077d30070ceeee3c84258bdca206cc6a
Mauro S S Goncalves <133293974+mrmssg@users.noreply.github.com>
2025-05-26 18:34:05 +0000

    001

--- main.js
+++ main.js
@@ -16,8 +16,9 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-05-20T00:00:00"); 
+let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 
 
