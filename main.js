ff2da208d5b781c1a5e56913d364a1d4810e4c21
Mauro S S Goncalves <133293974+mrmssg@users.noreply.github.com>
2025-07-28 20:59:31 +0000

    002

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2025-12-15T00:00:00");
-const tempoObjetivo4 = new Date("2026-02-01T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-08T00:00:00");
+const tempoObjetivo4 = new Date("2025-12-15T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
